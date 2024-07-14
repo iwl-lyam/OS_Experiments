61ffa3414914e0a14fdfb44664284ad882e5f170
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-07-14 10:09:29 +0100

    New build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git pull
 echo "preprocessing"
 ./helpers/generate_isrs.sh isrs_gen.c isrs_gen.inc
 echo "assembling bootloader"
-nasm "boot.asm" -f elf -g -F dwarf -o "boot.elf"
+nasm "boot.asm" -f bin -g -F dwarf -o "boot.bin"
 echo "assembling support files"
 nasm "kernel_entry.asm" -f elf -g -F dwarf -o "kernel_entry.o"
 nasm "idt.asm" -f elf -g -F dwarf -o "idt_asm.o"
@@ -17,6 +17,6 @@ i686-elf-gcc -ffreestanding -m32 -c -g "io.c" -o "io.o"
 #echo "zeroes assembly"
 #nasm "zeroes.asm" -f elf -g -F dwarf -o "zeroes.bin"
 echo "kernel link"
-i686-elf-ld -o "full_kernel.o" -Ttext 0x1000 "kernel_entry.o"  "idt.o" "pic.o" "io.o" "isr.o" "isr_asm.o" "idt_asm.o" "kernel.o" --oformat elf
-i686-elf-ld -o "OS.o" "boot.o" "full_kernel.o"
+i686-elf-ld -o "full_kernel.o" -Ttext 0x1000 "kernel_entry.o"  "idt.o" "pic.o" "io.o" "isr.o" "isr_asm.o" "idt_asm.o" "kernel.o" --oformat elf32
+i686-elf-ld -o "OS.o" "boot.bin" "full_kernel.o"
 #cat "boot.bin" "full_kernel.bin" "zeroes.bin"  > "OS.bin"
