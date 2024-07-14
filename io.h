b4aa2cc5fc9f6b792347291453ef75136fcb6e3c
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-07-14 09:54:20 +0100

    Fix types

--- io.h
+++ io.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-inline uint8_t inb(uint16_t port);
-inline void io_wait(void);
-inline void outb(uint16_t port, uint8_t val);
+uint8_t inb(uint16_t port);
+void io_wait(void);
+void outb(uint16_t port, uint8_t val);
